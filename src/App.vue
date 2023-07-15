f17a64469ab8202b9b109caa1df6f6ead1daaefb
takaaki <red.ducati.mac2255@gmail.com>
2023-07-15 12:59:45 +0900

    メッセージ変更

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import TheWelcome from "./components/TheWelcome.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="ともすた" />
+      <HelloWorld msg="hello" />
     </div>
   </header>
 
