e5f86ba71c7782e227bbc1cb261165d6d38b8a09
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-07-16 14:04:27 +0900

    v-コマンド

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,18 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+// import { computed } from "vue";
+
+import WelcomeItemVue from "./components/WelcomeItem.vue";
+
+// const answer = computed(() => {
+//   return 23 + 34;
+// });
+
+const message = "ようこそ　<strong>ともすた</strong>さん";
+const color = "white";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="11111" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Vue 3</h1>
+  <!-- <p>23 + 34 = {{ answer }}</p> -->
+  <p v-html="message" :style="{ color: color, 'background-color': 'blue' }"></p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
