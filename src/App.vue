c7d51377d347ca19d95dddbbf9273b18bf2f7fea
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-07-23 12:33:39 +0900

    v-modelの追加

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script setup>
-// import { computed } from "vue";
+import { ref, computed } from "vue";
 
 import WelcomeItemVue from "./components/WelcomeItem.vue";
 
-// const answer = computed(() => {
-//   return 23 + 34;
-// });
-
-const message = "ようこそ　<strong>ともすた</strong>さん";
+const message = computed(() => {
+  return "ようこそ " + myname.value + "さん";
+});
 const color = "white";
+const myname = ref("");
 </script>
 
 <template>
   <h1>Vue 3</h1>
   <!-- <p>23 + 34 = {{ answer }}</p> -->
+  <p>お名前は? : <input type="text" size="30" v-model="myname" /></p>
   <p v-html="message" :style="{ color: color, 'background-color': 'blue' }"></p>
 </template>
