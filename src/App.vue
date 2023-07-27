37afd6b8c1fbdfe74c437fbc812670396389e54c
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-07-27 16:09:44 +0900

    カウント機能作成

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref, computed } from "vue";
 
 import WelcomeItemVue from "./components/WelcomeItem.vue";
@@ -8,11 +8,23 @@ const message = computed(() => {
 });
 const color = "white";
 const myname = ref("");
+</script> -->
+
+<!-- <template>
+  <h1>Vue 3</h1>
+  <p>23 + 34 = {{ answer }}</p>
+  <p>お名前は? : <input type="text" size="30" v-model="myname" /></p>
+  <p v-html="message" :style="{ color: color, 'background-color': 'blue' }"></p>
+</template> -->
+<script setup>
+import { ref } from "vue";
+const number = ref(0);
+const add = () => {
+  number.value++;
+};
 </script>
 
 <template>
-  <h1>Vue 3</h1>
-  <!-- <p>23 + 34 = {{ answer }}</p> -->
-  <p>お名前は? : <input type="text" size="30" v-model="myname" /></p>
-  <p v-html="message" :style="{ color: color, 'background-color': 'blue' }"></p>
+  <input type="text" size="30" v-model="number" />
+  <button @click="add()">+1</button>
 </template>
