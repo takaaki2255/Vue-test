1443dd45bd8d7453c4922dcdff4bddf584438ca1
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-09-07 17:00:35 +0900

    ボタンアイコン作成

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!-- アプリ開発名　簡易型ジャンケンポン -->
+<script setup></script>
+
+<template>
+  <head>
+    <meta charset="utf-8" />
+    <title>rock paper scissors</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>じゃんけん　　ポンッ！ 最初に表示　　ボタン後消す</h1>
+      <div class="table">
+        <p>ボタンを押してね　最初に表示　　ボタン後消す</p>
+        <button class="btn">グー</button>
+        <button class="btn">チョキ</button>
+        <button class="btn">パー</button>
+      </div>
+      <div>
+        <p>勝ち　負け　引き分け　ボタンを押したら表示</p>
+      </div>
+    </div>
+  </body>
+  <footer></footer>
+</template>
