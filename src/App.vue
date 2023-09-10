7caf2d98ec40eec23e213fa67a62765394a8a57a
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-09-10 17:49:44 +0900

    レイアウト変更

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,56 @@
 <!-- アプリ開発名　簡易型ジャンケンポン -->
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const title = "じゃんけん　　ポンッ！ 最初に表示　　ボタン後消す";
+const message = "ボタンを押してね　最初に表示　　ボタン後消す";
+const answer = "勝ち　負け　引き分け　ボタンを押したら表示";
+// const handGu = ref([
+//   {
+//     name: "グー",
+//     image: "./public/image/Gu.png",
+//     click: false,
+//   },
+// ]);
+// const handChoki = ref([
+//   {
+//     name: "チョキ",
+//     image: "./public/image/choki.png",
+//     click: false,
+//   },
+// ]);
+// const handPa = ref([
+//   {
+//     id: 3,
+//     name: "パー",
+//     image: "./public/image/pa.png",
+//     click: false,
+//   },
+// ]);
+</script>
 
 <template>
   <head>
     <meta charset="utf-8" />
     <title>rock paper scissors</title>
   </head>
+
   <body>
+    <header>
+      <h3>じゃんけんをしましょう</h3>
+    </header>
     <div class="container">
-      <h1>じゃんけん　　ポンッ！ 最初に表示　　ボタン後消す</h1>
+      <h1 v-html="title"></h1>
+      <p v-html="message"></p>
       <div class="table">
-        <p>ボタンを押してね　最初に表示　　ボタン後消す</p>
         <button class="btn">グー</button>
         <button class="btn">チョキ</button>
         <button class="btn">パー</button>
       </div>
-      <div>
-        <p>勝ち　負け　引き分け　ボタンを押したら表示</p>
-      </div>
+      <p v-html="answer"></p>
     </div>
+    <footer>
+      <h2>勝てたら良い事があるかもね‼︎</h2>
+    </footer>
   </body>
-  <footer></footer>
 </template>
